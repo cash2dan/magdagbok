e719f200497deae3522ed70f30089287b97f81bb
cash2dan <daniel.jonsson74@gmail.com>
2026-04-27 18:51:47 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/magdagbok/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
